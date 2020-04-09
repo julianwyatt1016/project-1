219cc512ec6bcdec3e41644ce419fffb96d17883
Julian Wyatt <julianwyatt@Julians-MBP-2.domain>
2020-04-08 20:14:14 -0700

    Add sumEvens to funtions.js.

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -14,7 +14,6 @@ let arrAvg = (arr) => {
 arrAvg([2,3,5,7,9]);
 
 //Req. 3c
-
 let arrMax = (arr) => {
   let num = 0;
 
@@ -27,3 +26,18 @@ return num;
 };
 
 arrMax([2,3,5,7,9]);
+
+
+//Req. 3d
+let sumEvens = (arr) => {
+  let sum = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sum = sum + arr[i];
+    }
+  }
+  return sum;
+};
+
+sumEvens([1,2,3,4,5,6]); //12
