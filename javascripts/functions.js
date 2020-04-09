6b3cbfaf7f686bbcc5323ec14be322d5d650fdc6
Julian Wyatt <julianwyatt@Julians-MBP-2.domain>
2020-04-08 19:50:33 -0700

    Add arrAvg to functions.js. 

---
+++ javascripts/functions.js
@@ -0,0 +1,20 @@
+//jshint esversion 6
+
+//Req. 3b
+let arrAvg = (arr) => {
+  let sum = 0;
+
+  for (var i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  var avg = sum / arr.length;
+  return avg;
+};
+
+arrAvg([2,3,5,7,9]);
+
+//Req. 3c
+
+//let arrMax = (arr) {
+  //
+//};
